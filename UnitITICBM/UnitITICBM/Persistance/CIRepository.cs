a76b24f4dcbc19e581c4e4e6f2d35012d2891e04
DanielH76 <daniel.hammershoi@gmail.com>
2021-04-22 13:25:30 +0200

    Add funktion i CIRepo
    Tilføjet mulig implementering af Add funktion i CIRepositoryDB

--- UnitITICBM/UnitITICBM/Persistance/CIRepository.cs
+++ UnitITICBM/UnitITICBM/Persistance/CIRepository.cs
@@ -1,17 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 using UnitITICBM.Models;
 
 namespace UnitITICBM.Persistance
 {
-    public class CIRepository
+    public class CIRepository: IRepo<CI>
     {
         private List<CI> cIs;
+        private const string _tableName = "CIs";
 
         public CIRepository()
         {
             cIs = new List<CI>();
+        }
+
+        public void Add(CI item)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString.connectionString))
+            {
+                //Opens DB connection
+                conn.Open();
+
+                //Saves data to table
+                SqlCommand cmd = new SqlCommand($"INSERT INTO {_tableName}(CI_ID) VALUES (@CI_ID)", conn);
+                cmd.Parameters.Add("@CaliperID", System.Data.SqlDbType.Int).Value = item.CI_ID;
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CI GetAll(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CI> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Refresh()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(CI fromItem, CI toItem)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ UnitITICBM/UnitITICBM/Persistance/ConnectionString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitITICBM.Persistance
+{
+    public static class ConnectionString
+    {
+        public static string connectionString = "Server=10.56.8.35;Database=B_DB07_2020;User Id=B_STUDENT07;Password=B_OPENDB07";
+    }
+}
