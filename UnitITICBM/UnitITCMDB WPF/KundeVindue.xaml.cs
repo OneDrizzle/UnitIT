3d1a7c2488237614ff7f92bce34bee7f311a389c
Andreas Sørensen <70892717+OneDrizzle@users.noreply.github.com>
2021-04-28 10:41:35 +0200

    Tilføjet kunde GUI
    Kunde GUI er lavet med Listbox, UnitIT's logo er også i selve programmet nu så alle kan bruge det

---
+++ UnitITICBM/UnitITCMDB WPF/KundeVindue.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UnitITCMDB_WPF
+{
+    /// <summary>
+    /// Interaction logic for KundeVindue.xaml
+    /// </summary>
+    public partial class KundeVindue : Window
+    {
+        public KundeVindue()
+        {
+            InitializeComponent();
+        }
+    }
+}
