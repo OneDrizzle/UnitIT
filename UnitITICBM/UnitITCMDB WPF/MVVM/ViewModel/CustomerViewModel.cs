60282d54dca5cac9d04c9a6ce65454ab145eabd9
Alexander Bay <basket10bay@hotmail.com>
2021-04-30 11:10:05 +0200

    UpdatedGui

---
+++ UnitITICBM/UnitITCMDB WPF/MVVM/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitITCMDB_WPF.MVVM.ViewModel
+{
+    class CustomerViewModel
+    {
+    }
+}
---
+++ UnitITICBM/UnitITCMDB WPF/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitITCMDB_WPF.Core;
+
+namespace UnitITCMDB_WPF.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand CustomerViewCommand { get; set; }
+
+        public HomeViewModel HomeVm { get; set; }
+        public CustomerViewModel CustomerVm { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            CustomerVm = new CustomerViewModel();
+
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVm;
+            });
+
+            CustomerViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = CustomerVm;
+            });
+        }
+    }
+}
