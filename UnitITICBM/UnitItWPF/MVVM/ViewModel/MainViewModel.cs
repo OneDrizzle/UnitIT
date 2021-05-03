9f6f113365e2804c4e58a1b9047d2b88167965ac
Alexander Bay <basket10bay@hotmail.com>
2021-05-03 09:30:29 +0200

    Gui Update
    Tilføjet window switch funktion - mangler binding til c# metoder

---
+++ UnitITICBM/UnitItWPF/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitItWPF.Core;
+
+namespace UnitItWPF.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand CustomerViewCommand { get; set; }
+
+        public HomeViewModel HomeVm { get; set; }
+
+        public CustomerViewModel CustomerVm { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            CustomerVm = new CustomerViewModel();
+
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVm;
+            });
+
+            CustomerViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = CustomerVm;
+            });
+        }
+    }
+}
