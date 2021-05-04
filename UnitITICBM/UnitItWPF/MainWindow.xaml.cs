1cd464543593268a4a2e02e9cec191aa1f2f10ff
Alexander Bay <basket10bay@hotmail.com>
2021-05-04 16:30:43 +0200

    Gridbox med Database Connection

--- UnitITICBM/UnitItWPF/MainWindow.xaml.cs
+++ UnitITICBM/UnitItWPF/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace UnitItWPF
         {
             InitializeComponent();
         }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
