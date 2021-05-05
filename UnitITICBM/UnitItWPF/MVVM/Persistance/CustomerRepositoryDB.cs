881c4e226983ef893e1a5ed4e886c67ab91b8434
DanielH76 <daniel.hammershoi@gmail.com>
2021-05-05 13:47:34 +0200

    GetAllCustomers

--- UnitITICBM/UnitItWPF/MVVM/Persistance/CustomerRepositoryDB.cs
+++ UnitITICBM/UnitItWPF/MVVM/Persistance/CustomerRepositoryDB.cs
@@ -6,7 +6,7 @@ using UnitITICBM.Models;
 
 namespace UnitITICBM.Persistance
 {
-    public class CustomerRepositoryDB:IRepo<Customer>
+    public class CustomerRepositoryDB : IRepo<Customer>
     {
         private List<Customer> customers;
         public CustomerRepositoryDB()
@@ -46,9 +46,25 @@ namespace UnitITICBM.Persistance
         }
         public List<Customer> GetAll()
         {
-            throw new NotImplementedException();
-        }
+            using (SqlConnection conn = new SqlConnection(ConnectionString.connectionString))
+            {
+                conn.Open();
+                List<Customer> customersToFind = new List<Customer>();
+                string commandText = "SELECT * FROM CUSTOMERS";
 
+                SqlCommand cmd = new SqlCommand(commandText, conn);
+                SqlDataReader reader;
+                using (reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        customersToFind.Add(new Customer((int)reader[0], (string)reader[1]));
+                    }
+                }
+                return customersToFind;
+            }
+
+        }
         public void Refresh()
         {
             throw new NotImplementedException();
